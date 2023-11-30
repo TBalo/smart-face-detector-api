22887abdd6a4fd36335b4977ddfdd25e65ef6e8d
TBalo <tbalog426@gmail.com>
2023-11-30 04:24:24 +0100

    added backend apis

---
+++ controller/image.js
@@ -0,0 +1,63 @@
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id )
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+const PAT = "88914030ce0d4fbc9efa84f4f524b75c";
+const USER_ID = "tbalo";
+const APP_ID = "my-first-application-3y823b";
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+
+
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key " + PAT);
+
+const handleApiCall = (req, res) => {
+    const IMAGE_URL  = req.body.input;
+stub.PostModelOutputs(
+    {
+        user_app_id: {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        model_id: MODEL_ID,
+        version_id: MODEL_VERSION_ID, 
+        inputs: [
+            { data: { image: { url: IMAGE_URL, allow_duplicate_url: true } } }
+        ]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            throw new Error(err);
+        }
+
+        if (response.status.code !== 10000) {
+            throw new Error("Post model outputs failed, status: " + response.status.description);
+        }
+
+        const output = response.outputs[0];
+
+        console.log("Predicted concepts:");
+        for (const concept of output.data.concepts) {
+            console.log(concept.name + " " + concept.value);
+        }
+        res.json(response)
+    }
+);
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+};
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const knex = require('knex');
+
+const register = require('./controller/register');
+const signin = require('./controller/signin');
+const profile = require('./controller/profile');
+const image = require('./controller/image');
+const imageurl = require('./controller/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'TBALO',
+      password : '',
+      database : 'smart-brain'
+    },
+    useNullAsDefault: true
+  });
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id: "123",
+            name: "John",
+            email: "john@gmail.com",
+            password: "mangoes",
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: "124",
+            name: "Sally",
+            email: "sally@gmail.com",
+            password: "bananas",
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+
+    login: [
+        {
+            id: '',
+            hash: '',
+            password: ''
+        }
+    ]
+};
+app.get('/', (req, res) => {res.send(database.users);})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
+
+PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
+});
+
+console.log(process.env);
